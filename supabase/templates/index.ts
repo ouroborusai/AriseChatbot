82788ee3e8110a381d5a4b18af5e3103997e4996
MTZ Dev <dev@mtz.cl>
2026-04-12 01:03:32 -0400

    fix(templates): inyectar nodos json 02 al systemTemplates de nextjs para permitar recuperacion en produccion

--- supabase/templates/index.ts
+++ supabase/templates/index.ts
@@ -1,7 +1,15 @@
 import prospecto from './01_bienvenida/01_A_bienvenida_prospecto.json';
 import cliente from './01_bienvenida/01_B_menu_principal_cliente.json';
 
+// Flujo Prospectos
+import prospecto_cotizacion from './02_prospectos/02_A_prospecto_cotizacion.json';
+import prospecto_faq from './02_prospectos/02_B_prospecto_faq.json';
+import derivacion_asesor from './02_prospectos/02_C_derivacion_asesor.json';
+
 export const systemTemplates = [
   prospecto,
-  cliente
+  cliente,
+  prospecto_cotizacion,
+  prospecto_faq,
+  derivacion_asesor
 ];
